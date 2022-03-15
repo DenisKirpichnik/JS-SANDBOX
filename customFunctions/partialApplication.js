a46a2a53ab1dbc504741fca1b4229b68a9daa94d
Denis Kirpichnik <d.kirpichnik>
2022-03-15 15:03:42 +0300

    cool

--- customFunctions/partialApplication.js
+++ customFunctions/partialApplication.js
@@ -7,3 +7,14 @@ function mul(a, b) {
   alert( double(3) ); // = mul(2, 3) = 6
   alert( double(4) ); // = mul(2, 4) = 8
   alert( double(5) ); // = mul(2, 5) = 10
+
+  function compareName<T extends {name:string}>(obj1: T, obj2: T): void{
+     
+    if(obj1.name === obj2.name){
+        console.log("Имена совпадают");
+    }
+    else{
+        console.log("Имена различаются");
+    }
+ 
+}
